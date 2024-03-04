aa87f4a9005a97131e034103dd261cb0b3723dd4
HoangSon1998Beos <sonhdev97@gmail.com>
2024-03-04 10:16:15 +0700

    sfsd

---
+++ PJ_EVN/src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import Login from './Login.vue'
+
+const app = createApp(Login)
+app.mount('#app')
+
+// import 'vuetify/dist/vuetify.css'
+// import Vuetify from 'vuetify'
+
+// app.use(Vuetify)
+// const vuetify = new Vuetify()
+
+// app.component('Login', Login)
+
+// app.use(vuetify)
+
