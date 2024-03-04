8d2cd8453e6f80d8d14556f4e5417d6528c14a08
DucThan25 <nguyenducthan2507@gmail.com>
2024-03-04 14:01:46 +0700

    add vuetify

--- PJ_EVN/src/components/HelloWorld.vue
+++ PJ_EVN/src/components/HelloWorld.vue
@@ -1,40 +1,64 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+ <v-app>
+    <v-btn color="primary">Primary</v-btn>
+  </v-app>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<script>
+import logo from '../assets/logo.svg'
+
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+    ecosystem: [
+      {
+        text: 'vuetify-loader',
+        href: 'https://github.com/vuetifyjs/vuetify-loader/tree/next',
+      },
+      {
+        text: 'github',
+        href: 'https://github.com/vuetifyjs/vuetify/tree/next',
+      },
+      {
+        text: 'awesome-vuetify',
+        href: 'https://github.com/vuetifyjs/awesome-vuetify',
+      },
+    ],
+    importantLinks: [
+      {
+        text: 'Chat',
+        href: 'https://community.vuetifyjs.com',
+      },
+      {
+        text: 'Made with Vuetify',
+        href: 'https://madewithvuejs.com/vuetify',
+      },
+      {
+        text: 'Twitter',
+        href: 'https://twitter.com/vuetifyjs',
+      },
+      {
+        text: 'Articles',
+        href: 'https://medium.com/vuetify',
+      },
+    ],
+    logo,
+    whatsNext: [
+      {
+        text: 'Explore components',
+        href: 'https://vuetifyjs.com',
+      },
+      {
+        text: 'Roadmap',
+        href: 'https://vuetifyjs.com/introduction/roadmap/',
+      },
+      {
+        text: 'Frequently Asked Questions',
+        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+      },
+    ],
+  }),
 }
-</style>
+</script>
--- PJ_EVN/src/main.js
+++ PJ_EVN/src/main.js
@@ -1,18 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
-import Login from './Login.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-const app = createApp(Login)
-app.mount('#app')
+loadFonts()
 
-// import 'vuetify/dist/vuetify.css'
-// import Vuetify from 'vuetify'
-
-// app.use(Vuetify)
-// const vuetify = new Vuetify()
-
-// app.component('Login', Login)
-
-// app.use(vuetify)
-
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
